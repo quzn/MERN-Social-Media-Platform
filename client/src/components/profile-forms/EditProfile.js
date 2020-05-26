cdf1f26f15a46b0312f9e7ae2ffa3d5eab18d5df
Cole O'Brien <gastrocole@gmail.com>
2020-05-26 17:20:49 +0100

    Add edit profile component

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,11 +1,18 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { updateOrCreateProfile } from '../../actions/profile';
-import profile from '../../reducers/profile';
+import {
+  updateOrCreateProfile,
+  getCurrentProfile,
+} from '../../actions/profile';
 
-const CreateProfile = ({ updateOrCreateProfile, history }) => {
+const EditProfile = ({
+  profile: { profile, loading },
+  updateOrCreateProfile,
+  getCurrentProfile,
+  history,
+}) => {
   const [formData, setFormData] = useState({
     company: ' ',
     website: '',
@@ -22,6 +29,26 @@ const CreateProfile = ({ updateOrCreateProfile, history }) => {
   });
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      company: loading || !profile.company ? '' : profile.company,
+      website: loading || !profile.website ? '' : profile.website,
+      location: loading || !profile.location ? '' : profile.location,
+      status: loading || !profile.status ? '' : profile.status,
+      skills: loading || !profile.skills ? '' : profile.skills.join(','),
+      githubusername:
+        loading || !profile.githubusername ? '' : profile.githubusername,
+      bio: loading || !profile.bio ? '' : profile.bio,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social ? '' : profile.social.youtube,
+      instagram: loading || !profile.social ? '' : profile.social.instagram,
+    });
+  }, [loading]);
 
   const {
     company,
@@ -43,7 +70,7 @@ const CreateProfile = ({ updateOrCreateProfile, history }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    updateOrCreateProfile(formData, history);
+    updateOrCreateProfile(formData, history, true);
   };
 
   return (
@@ -230,10 +257,17 @@ const CreateProfile = ({ updateOrCreateProfile, history }) => {
   );
 };
 
-CreateProfile.propTypes = {
+EditProfile.propTypes = {
   updateOrCreateProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
-export default connect(null, { updateOrCreateProfile })(
-  withRouter(CreateProfile)
-);
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, {
+  updateOrCreateProfile,
+  getCurrentProfile,
+})(withRouter(EditProfile));

9bb337936caee338e8d7ec908571d26b06e31b5d
Cole O'Brien <gastrocole@gmail.com>
2020-05-26 16:43:12 +0100

    Added get profile action, create and edit profile action, and edit profile component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,8 +11,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         <Link to='/profiles'>Developers</Link>
       </li>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>
+          {'  '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <Link onClick={logout} to='/'>
-          <i className='fas fa-sign-out-alt'></i>{' '}
+          <i className='fas fa-sign-out-alt'></i>
+          {'  '}
           <span className='hide-sm'>Logout</span>
         </Link>
       </li>
